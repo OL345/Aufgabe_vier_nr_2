aa3334b82d0ce1f9b6b2a7bd0653b0deccbfbc36
Leo Oberhollenzer <105490336+Lecho345@users.noreply.github.com>
2022-10-17 15:13:01 +0200

    e

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,16 @@ import zoo.fisch.*;
 import zoo.saugetier.*;
 import zoo.vogel.*;
 import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Lachs lachs = new Lachs("Fisch", "grün");
+        Lachs lachs = new Lachs("Lachs", "grün");
         Hai hai = new Hai("Hai");
         Bieber bieber = new Bieber("Bieber", "Türkis");
         Ozelot ozelot = new Ozelot("Ozelot", "schwarz");
         Papagei papagei = new Papagei("Papagei", "gelb");
         Rabe rabe = new Rabe("Rabe", "pink");
+
         while (true) {
             System.out.println("\n1. Fische");
             System.out.println("2. Vögel");
@@ -24,46 +23,44 @@ public class Main {
                     System.out.println("\n1. Lachs");
                     System.out.println("2. Hai");
                     System.out.println("3. Exit");
-                    int input2 = scanner.nextInt();
-                    if (input2 == 1) {
+                    input = scanner.nextInt();
+                    if (input == 1) {
                         System.out.println("\n1. Make sound");
                         System.out.println("2. Greet");
                         System.out.println("3. Eat");
                         System.out.println("4. Swim");
                         System.out.println("5. Exit");
-                        int input3 = scanner.nextInt();
-                        if (input3 == 1) {
+                        input = scanner.nextInt();
+                        if (input == 1) {
                             lachs.makeSound();
-                        } else if (input3 == 2) {
+                        } else if (input == 2) {
                             lachs.greet(lachs);
-                        } else if (input3 == 3) {
+                        } else if (input == 3) {
                             lachs.eat(100);
-                        } else if (input3 == 4) {
+                        } else if (input == 4) {
                             lachs.swim();
-                        } else if (input3 == 5) {
+                        } else if (input == 5) {
                             System.exit(0);
                         }
-                    } else if (input2 == 2) {
+                    } else if (input == 2) {
                         System.out.println("\n1. Make sound");
                         System.out.println("2. Greet");
                         System.out.println("3. Eat");
                         System.out.println("4. Swim");
                         System.out.println("5. Hunt");
                         System.out.println("6. Exit");
-                        int input3 = scanner.nextInt();
-                        if (input3 == 1) {
+                        input = scanner.nextInt();
+                        if (input == 1) {
                             hai.makeSound();
-                        } else if (input3 == 2) {
+                        } else if (input == 2) {
                             hai.greet(hai);
-                        } else if (input3 == 3) {
+                        } else if (input == 3) {
                             hai.eat(100);
-                        } else if (input3 == 4) {
+                        } else if (input == 4) {
                             hai.swim();
-                        } else if (input3 == 5) {
+                        } else if (input == 5) {
                             hai.hunt(lachs);
-                        } else if (input3 == 6) {
-                            System.exit(0);
-                        } else if (input3 == 6) {
+                        } else if (input == 6) {
                             System.exit(0);
                         }
                     }
@@ -72,44 +69,44 @@ public class Main {
                     System.out.println("\n1. Rabe");
                     System.out.println("2. Papagei");
                     System.out.println("3. Exit");
-                    int input4 = scanner.nextInt();
-                    if (input4 == 1) {
+                    input = scanner.nextInt();
+                    if (input == 1) {
                         System.out.println("\n1. Make sound");
                         System.out.println("2. Greet");
                         System.out.println("3. Eat");
                         System.out.println("4. Fly");
                         System.out.println("5. Exit");
-                        int input5 = scanner.nextInt();
-                        if (input5 == 1) {
+                        input = scanner.nextInt();
+                        if (input == 1) {
                             rabe.makeSound();
-                        } else if (input5 == 2) {
+                        } else if (input == 2) {
                             rabe.greet(papagei);
-                        } else if (input5 == 3) {
+                        } else if (input == 3) {
                             rabe.eat(100);
-                        } else if (input5 == 4) {
+                        } else if (input == 4) {
                             rabe.fly();
-                        } else if (input5 == 5) {
-                            break;
+                        } else if (input == 5) {
+                            System.exit(0);
                         }
-                    } else if (input4 == 2) {
+                    } else if (input == 2) {
                         System.out.println("\n1. Make sound");
                         System.out.println("2. Greet");
                         System.out.println("3. Eat");
                         System.out.println("4. Fly");
                         System.out.println("5. Exit");
-                        int input5 = scanner.nextInt();
-                        if (input5 == 1) {
+                        input = scanner.nextInt();
+                        if (input == 1) {
                             papagei.makeSound();
-                        } else if (input5 == 2) {
+                        } else if (input == 2) {
                             papagei.greet(papagei);
-                        } else if (input5 == 3) {
+                        } else if (input == 3) {
                             papagei.eat(100);
-                        } else if (input5 == 4) {
+                        } else if (input == 4) {
                             papagei.fly();
-                        } else if (input5 == 5) {
-                            break;
+                        } else if (input == 5) {
+                            System.exit(0);
                         }
-                    } else if (input4 == 3) {
+                    } else if (input == 3) {
                         System.exit(0);
                     }
                 }
@@ -117,55 +114,50 @@ public class Main {
                     System.out.println("\n1. Bieber");
                     System.out.println("2. Ozelot");
                     System.out.println("3. Exit");
-                    int input6 = scanner.nextInt();
-                    if (input6 == 1) {
+                    input = scanner.nextInt();
+                    if (input == 1) {
                         System.out.println("\n1. Make sound");
                         System.out.println("2. Greet");
                         System.out.println("3. Eat");
                         System.out.println("4. Run");
                         System.out.println("5. Exit");
-                        int input7 = scanner.nextInt();
-                        if (input7 == 1) {
+                        input = scanner.nextInt();
+                        if (input == 1) {
                             bieber.makeSound();
-                        } else if (input7 == 2) {
-                            bieber.greet(bieber);
-                        } else if (input7 == 3) {
-                            bieber.eat(100);
-                        } else if (input7 == 4) {
-                            bieber.run();
-                        } else if (input7 == 5) {
+                        } else if (input == 2) {
+                            rabe.greet(papagei);
+                        } else if (input == 3) {
+                            rabe.eat(100);
+                        } else if (input == 4) {
+                            rabe.fly();
+                        } else if (input == 5) {
                             System.exit(0);
                         }
-                    } else if (input6 == 2) {
+                    } else if (input == 2) {
                         System.out.println("\n1. Make sound");
                         System.out.println("2. Greet");
                         System.out.println("3. Eat");
-                        System.out.println("4. Run");
-                        System.out.println("5. Hunt");
-                        System.out.println("6. Exit");
-                        int input7 = scanner.nextInt();
-                        if (input7 == 1) {
+                        System.out.println("4. Hunt");
+                        System.out.println("5. Exit");
+                        input = scanner.nextInt();
+                        if (input == 1) {
                             ozelot.makeSound();
-                        } else if (input7 == 2) {
-                            ozelot.greet(ozelot);
-                        } else if (input7 == 3) {
+                        } else if (input == 2) {
+                            ozelot.greet(papagei);
+                        } else if (input == 3) {
                             ozelot.eat(100);
-                        } else if (input7 == 4) {
-                            ozelot.run();
-                        } else if (input7 == 5) {
+                        } else if (input == 4) {
                             ozelot.hunt(bieber);
-                        } else if (input7 == 6) {
+                        } else if (input == 5) {
                             System.exit(0);
                         }
-
-                    } else if (input6 == 3) {
+                    } else if (input == 3) {
                         System.exit(0);
                     }
                 }
+                case 4 -> System.exit(0);
             }
-
         }
-
     }
-}
 
+}
--- src/zoo/saugetier/Saugetier.java
+++ src/zoo/saugetier/Saugetier.java
@@ -1,18 +1,15 @@
 package zoo.saugetier;
-
 import zoo.Tier;
-import zoo.vogel.Papagei;
-
-import java.util.Random;
 import java.util.Scanner;
 
 public class Saugetier extends Tier {
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
     int input = scanner.nextInt();
     int input2 = scanner.nextInt();
     protected Saugetier(final String name, final String color) {
         super(name, color);
     }
+
     public void makeSound() {
         System.out.println("Tier " + getName() + " macht Geräusche.");
     }
